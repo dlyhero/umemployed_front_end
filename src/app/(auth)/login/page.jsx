e0e0d51f0fcf13b2cf99485134c4e8b630a41b8f
kinsmanEnu <kinsman_enu18@yahoo.com>
2025-03-22 15:00:43 +0100

    signin page setting up the size

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,50 +1,65 @@
-import React from 'react'
-import { FaApple, FaGoogle, FaHome } from "react-icons/fa";
-import Form from './Form'
+"use client"
+import { Button } from '@/src/components/ui/button'
+import React, { useEffect, useState } from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { FaEye, FaEyeSlash } from 'react-icons/fa'
+import { z } from "zod";
+import { useForm } from 'react-hook-form'
 
-export const metadata = {
-  title: "Sign Up - UmEmployed",
-};
+const loginSchema = z.object({
+    email: z
+    .string()
+    .min(1, { message: "must be at least one character" })
+    .max(254, { message: "Email must be less than 255 characters" })
+    .email("This is not a valid email"),
+    password: z
+        .string()
+        .min(8, { message: "Password must be at least 8 characters" })
+        .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
+        .regex(/[0-9]/, { message: "Password must contain at least one number" })
+        .nonempty({ message: "Password is required" }),
+});
 
-function Login() {
-  return (
-    <div>
-      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 container max-w-6xl mx-auto lg:flex items-center'>
-        <div id='left-section' className='hidden lg:block'>
-          <img src="/images/login.webp" alt="" />
-        </div>
-        <div id='right-section' className='max-w-xl mx-auto lg:w-[60%] p-4  rounded-lg'>
-          <div className='flex flex-col items-center w-full'>
-            <div className="top-section w-full text-center">
-              <div className="border-gray-300 p-4 gap-2">
-                <div className="heading">
-                  <h1 className='text-gray-800 font-black text:2xl lg:text-3xl'>Welcome Back!</h1>
-                  <h2 className="text-gray-500">Please log in to your account</h2>
-                </div>
-              </div>
-              <div id="alt-auth" className="w-full">
-                <div className="relative wrapper shadow border border-gray-300 p-2 h-fit rounded-lg">
-                  <img src="/icons/google.png" alt="google-icon" className='h-6 w-6' />
-                  <span className='absolute left-1/2 top-1/2 transform -translate-1/2 -translate-y-1/2 text-gray-700 font-semibold'>Sign in with google</span>
-                </div>
-              </div>
+
+export default function Form() {
+    const [passwordVisibilty, setPasswordVisibility] = useState(false);
+    const { register, handleSubmit, formState: { errors } } = useForm({ resolver: zodResolver(loginSchema) })
+
+    const handleVisibility = (e) => {
+        setPasswordVisibility((prev) => !prev);
+    };
+
+    const onSubmit = data => {
+        console.log(data);
+    }
+
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className='w-full'>
+            <div className="email-wrap flex flex-col gap-1">
+                <label htmlFor="email" className='ml-1 text-gray-500 font-semibold'>Email</label>
+                <input {...register('email')} type="email" className='p-2 border border-gray-300 outline-none rounded-lg bg-transparent' id='email' name='email' placeholder='Enter your email' />
+                {errors.email && <p className='text-red-500 mt-1 ml-1 italic'>{errors.email.message}</p>}
             </div>
-            <div className="flex items-center my-4 w-full">
-              <hr className="flex-grow border-gray-300" />
-              <span className="px-3 text-gray-500 text-sm">or</span>
-              <hr className="flex-grow border-gray-300" />
+            <div className="password-wrap flex flex-col gap-1 my-4 relative">
+                <label htmlFor="password" className='ml-1 text-gray-500 font-semibold'>Password</label>
+                <input {...register('password')} type={`${!passwordVisibilty ? 'password' : 'text'}`} className='p-2 border border-gray-300 outline-none rounded-lg bg-transparent' id='password' name='password' placeholder='Enter your password' />
+                {errors.password && <p className='text-red-500 mt-1 ml-1 italic'>{errors.password.message}</p>}
+                <Button variant={'ghost'} className='absolute top-7 right-3 text-gray-700 border-none' onClick={handleVisibility} >{passwordVisibilty ? <FaEyeSlash /> : <FaEye />}</Button>
             </div>
-          <div className="bottom-section w-full">
-           <Form />
-           <div className='mt-2 text-center'>
-            <span className='text-gray-5000'>Don't have an account? </span><span className='text-[#1e90ff]'>Sign up</span>
-           </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div >
-  )
+            <div className="flex items-center justify-between">
+                <div className='check-btn space-x-2'>
+                    <input type="checkbox" name="rememberMe" id="rememberMe" />
+                    <span>Remember me</span>
+                </div>
+                <a href="#" className='text-[#1e90ff]'>Forgot your password?</a>
+            </div>
+            <div className="btn-wrap mt-4">
+                <Button variant={'brand'}  className={'font-semibold cursor-pointer'}>Log In</Button>
+            </div>
+        </form>
+    )
 }
-
-export default Login
