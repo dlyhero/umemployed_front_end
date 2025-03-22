22d491713e188d3a4dd6d4321b522fd28817646a
kinsmanEnu <kinsman_enu18@yahoo.com>
2025-03-22 11:16:41 +0100

    searbar added, header added, menu working properly

--- src/components/common/Header/AuthButtons.jsx
+++ src/components/common/Header/AuthButtons.jsx
@@ -1,7 +1,7 @@
 export default function AuthButtons() {
     return (
       <a
-        href="/api/users/login/"
+        href="/login"
         className="px-4 lg:px-5 py-2 lg:py-3 text-[#1e90ff] font-semibold border border-[#1e90ff] rounded-full"
       >
         Sign In
