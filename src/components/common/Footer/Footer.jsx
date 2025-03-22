b414780d980fbd7312b3ea0131ae35777d30a34c
kinsmanEnu <kinsman_enu18@yahoo.com>
2025-03-22 17:05:38 +0100

    footer setup in auth pages

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -1,28 +1,45 @@
-import React from 'react'
+import React from "react";
+import { FaTwitter, FaLinkedin, FaFacebook } from "react-icons/fa";
 
 export default function Footer() {
-  return (
-    <div class="flex flex-col sm:flex-row justify-center gap-2 items-center">
-    <p class="text-sm">© 2024 UmEmployed. All rights reserved.</p>
-    <div class="flex space-x-4 items-center">
-      <a href="/accounts/user/feature-not-implemented/" class="text-white hover:text-gray-300">
-        <i class="fab fa-twitter text-black"></i>
-      </a>
-      <a href="/accounts/user/feature-not-implemented/" class="text-white hover:text-gray-300">
-        <i class="fab fa-linkedin text-black"></i>
-      </a>
-      <a href="/accounts/user/feature-not-implemented/" class="text-white hover:text-gray-300">
-        <i class="fab fa-facebook text-black"></i>
-      </a>
-      <img src="/images/privacy.png" alt="" class="w-10 h-5" />
-    </div>
-    <div class="text-sm ">
-                    
-      <a href="/accounts/user/feature-not-implemented/" class="hover:text-gray-300">Privacy Policy
-        </a>
-      
-      <a href="/accounts/user/feature-not-implemented/" class="hover:text-gray-300">Contact Us</a>
-    </div>
-  </div>
-  )
+    return (
+        <div className="flex flex-col sm:flex-row justify-center gap-2 items-center absolute bottom-2 left-1/2 -translate-x-1/2 text-gray-600">
+            <p className="text-sm text-nowrap">© 2024 UmEmployed. All rights reserved.</p>
+            <div className="flex space-x-2 items-center">
+                <a
+                    href="/accounts/user/feature-not-implemented/"
+                    className="text-black hover:text-gray-500"
+                >
+                    <FaTwitter size={20} />
+                </a>
+                <a
+                    href="/accounts/user/feature-not-implemented/"
+                    className="text-black hover:text-gray-500"
+                >
+                    <FaLinkedin size={20} />
+                </a>
+                <a
+                    href="/accounts/user/feature-not-implemented/"
+                    className="text-black hover:text-gray-500"
+                >
+                    <FaFacebook size={20} />
+                </a>
+                <img src="/images/privacy.png" alt="Privacy" className="w-10 h-5" />
+            </div>
+            <div className="text-sm flex space-x-4">
+                <a
+                    href="/accounts/user/feature-not-implemented/"
+                    className="hover:text-gray-500"
+                >
+                    Privacy Policy
+                </a>
+                <a
+                    href="/accounts/user/feature-not-implemented/"
+                    className="hover:text-gray-500"
+                >
+                    Contact Us
+                </a>
+            </div>
+        </div>
+    );
 }
