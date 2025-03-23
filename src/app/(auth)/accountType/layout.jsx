6ecf60e8501a924d3eeaf9c7c5e49df175328eec
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-03-23 20:45:20 +0100

    feat:implemented the choose account tpye oage

---
+++ src/app/(auth)/accountType/layout.jsx
@@ -0,0 +1,33 @@
+// src/app/(auth)/accountType/layout.jsx
+import { Geist, Geist_Mono } from "next/font/google";
+import "../../globals.css";
+import Header from "@/src/components/common/Header/index";
+import Footer from '@/src/components/common/Footer/Footer';
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Account Type",
+};
+
+export default function AccountTypeLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-[15px]`}
+      >
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
