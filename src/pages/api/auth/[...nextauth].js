95185c1b70ccb851c7fe8d33ec6d107009683aed
Kinsman <clintonenu18@gmail.com>
2025-03-25 08:30:22 -0400

    resolving google auth issues

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -48,6 +48,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),   
   ],
+  debug: true,
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
