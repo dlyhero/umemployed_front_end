5d7c9ec2f0d90659964f38028151956e07d7fbc0
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-03-30 11:27:14 +0100

    Fix:Fix teh errors realted to the building process

--- src/app/companies/[companyId]/layout.jsx
+++ src/app/companies/[companyId]/layout.jsx
@@ -1,19 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "@/src/app/globals.css";
 import AuthProvider from "@/src/components/AuthProvider";
 import {Header} from "@/src/components/common/Header";
 import Footer from "@/src/components/common/Footer";
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "company list",
@@ -23,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased text-[15px]`}
+        className={`antialiased text-[15px]`}
       >  
         <AuthProvider>
           <Header />
