fb0b650164ca88d5aa3679a1f96f9a0083658c6b
Enu <clintonenu18@gmail.com>
2025-04-04 00:47:20 +0100

    setting up upload page

---
+++ src/app/applicant/upload/layout.jsx
@@ -0,0 +1,26 @@
+// src/app/(auth)/accountType/layout.jsx
+import { Header } from "@/src/components/common/Header";
+import "../../globals.css";
+import AuthProvider from "@/src/components/AuthProvider";
+
+
+
+
+export const metadata = {
+  title: "Account Type",
+};
+
+export default function AccountTypeLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={` antialiased text-[15px]`}
+      > 
+       <AuthProvider>
+       <Header />
+       {children}                                     
+       </AuthProvider>
+      </body>
+    </html>
+  );
+}
