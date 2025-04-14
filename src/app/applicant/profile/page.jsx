30c3e54cea2be6a2483e09921dbbde504f330e10
kinsman <clintonenu18@gmail.com>
2025-04-14 17:59:06 +0100

    updates on profile and dashboard

--- src/app/applicant/profile/page.jsx
+++ src/app/applicant/profile/page.jsx
@@ -8,7 +8,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { AlertCircle, Loader2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import baseUrl from "../../api/baseUrl";
+import baseUrl from "@/src/pages/api/auth/baseUrl";
 
 export default function ProfilePage() {
   const { data: session, status } = useSession();
