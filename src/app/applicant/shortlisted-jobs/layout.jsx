385b82f8712a81e172428ceafb93e1d95c981132
kinsmanEnu <clintonenu18@gmail.com>
2025-06-02 15:15:44 +0100

    select role page updated

---
+++ src/app/applicant/shortlisted-jobs/layout.jsx
@@ -0,0 +1,29 @@
+import "@/src/app/globals.css";
+import AuthProvider from "@/src/components/AuthProvider";
+import Footer3 from "@/src/components/common/Footer/Footer3";
+import {Header} from "@/src/components/common/Header";
+import { useSession } from "next-auth/react";
+import { Toaster } from "sonner";
+
+
+
+export const metadata = {
+  title: `Profile `,
+};
+
+
+
+export default function ShortListedJobsLayout({ children }) {
+  return (
+      <div
+        className={` antialiased text-[15px]`}
+      >  
+        <AuthProvider>
+          <Header />
+          {children}
+          <Footer3 />
+          <Toaster position="top-center" richColors/>
+        </AuthProvider>
+      </div>
+  );
+}
