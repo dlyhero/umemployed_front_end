2d93aea62fac5d7662d43793aaa50b625241aeb9
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-06-08 20:55:00 +0100

    fix

--- src/middleware.js
+++ src/middleware.js
@@ -97,6 +97,7 @@ export async function middleware(request) {
   return NextResponse.next();
 }
 
+
 export const config = {
   matcher: [
     '/((?!_next/static|_next/image|favicon.ico|images|api).*)',
